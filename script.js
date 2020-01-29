f17c7f5442c6d84218dd637a40633150a9a308ea
sill2673 <sill2673@stud.kea.dk>
2020-01-29 23:31:00 +0100

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+//$(Function()) {
+//    var $window = $(window),
+//        win_height_padded ? $window.height() * 1.1, isTouch = Modernizr.touch;
+//    if (isTouch) {
+//        $('.revealOnScroll').addClass('animated');
+//    }
+//}
+//
+//$window.on('scroll', revealOnScroll);
+//function revealOnScroll(){
+//    var scrolled = $window.scrollTop(), win_height_padded = $window.height()* 1.1;
+//    §('revealOnScroll:not(.animated)').each(function(){})
+//}
